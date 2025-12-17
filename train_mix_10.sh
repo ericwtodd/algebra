c21b249b465646759b1d88076524ed1f2627cacd
Eric Todd <eric.w.todd@gmail.com>
2025-12-17 10:37:51 -0500

    update train script

--- train_mix_10.sh
+++ train_mix_10.sh
@@ -18,6 +18,6 @@ fi
 python training.py \
     --d_model=1024 --d_mlp=4096 --block_size=1024 --n_layers=4 --n_heads=8 --leftpad=false \
     --lr=1e-5 --batch_size=128 --positional_encoding=rope \
-    --n_steps=200001 --lr_warmup_steps=1000 --evaluation_steps=200 --checkpoint_steps=5000 \
-    --task_name="$1" --k_shots=200 --seed=42\
+    --n_steps=200001 --lr_warmup_steps=1000 --evaluation_steps=100 --checkpoint_steps=5000 \
+    --task_name="$1" --k_shots=200 --seed=999\
     --task_config='{"num_symbols":16,"max_order":10,"mix":0.7,"holdout_zero":true}'
